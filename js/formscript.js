798d137425381780cb8b316969006748451595ef
les <les.lumb@it.ox.ac.uk>
2015-11-18 14:22:20 +0000

    report an incident form updated

--- js/formscript.js
+++ js/formscript.js
@@ -1,27 +1,63 @@
-      $(function(){
+$(function(){
 	  
 		/* these divs are hidden by javascript rather than css so that they remain visible if javascript is disabled  */
 		$(".hideme").hide(); 
 		
-	  $.viewMap = {
+	  $.viewMap1 = {
+		'0' : $([]),
+		'adminstaff' : $('#adminstaff'),
+		'nonadminstaff' : $('#nonadminstaff')	
+	  };
+	  	
+	  $('#selectPers').change(function() {
+          
+		// hide all
+		$.each($.viewMap1, function() { this.hide();
+        });
+          
+		// show selected
+		$.viewMap1[$(this).val()].show();
+          
+	  });
+	  
+	});
+
+
+$(function(){
+	  
+		/* these divs are hidden by javascript rather than css so that they remain visible if javascript is disabled  */
+		$(".hideme").hide(); 
+		
+	  $.viewMap2 = {
 		'0' : $([]),
 		'group1' : $('#group1'),
 		'group2' : $('#group2'),
 		'group3' : $('#group3'),
 		'group4' : $('#group4'),
 		'group5' : $('#group5'),
-		'group6' : $('#group6')		
+		'group6' : $('#group6'),	
+		'group7' : $('#group7')	
 	  };
 	  	
 	  $('#selectType').change(function() {
-		// hide all
-		$.each($.viewMap, function() { this.hide(); });
-		// show current
-		$.viewMap[$(this).val()].show();
-	  });
+          
+            // hide all
+            $.each($.viewMap2, function() { this.hide();
+            });
+
+            // show selected
+            $.viewMap2[$(this).val()].show();
+            if($(this).val() !== 'group7') {
+              $('#commonpart').show();
+            } else {
+              $('#commonpart').hide();
+            }
+
+          });
 	  
-	  // show thank you
-	  $('.submitbutton').click(function(){
-			$('#thanks').show();
+        // show thank you
+        $('.submitbutton').click(function(){
+        $('#thanks').show();
 		});
 	});
+
