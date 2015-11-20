deefa1c389914805a75d88641df8b8401f2e2d26
les <les.lumb@it.ox.ac.uk>
2015-11-20 11:37:10 +0000

    added in sidebox show/hide

---
+++ js/sidebox.js
@@ -0,0 +1,15 @@
+/**
+ * @sidebox.js
+ * function to hide sidebox links on policy and guidance page 
+**/
+
+
+jQuery(document).ready(function() {
+     
+          jQuery('.sidebox').click(function() {
+              
+            jQuery(this).toggleClass('sidebox-hide');
+            jQuery('.col-lg-8').toggleClass('col-lg-8-Expanded');                        
+          }); 
+ 
+});
